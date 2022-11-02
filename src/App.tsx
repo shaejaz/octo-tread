841cfe47add5a0ebc168bf2e8eecab9806ff6289
Shahmir Ejaz <shaejaz5@gmail.com>
2022-11-02 17:01:41 +0000

    Remove stylesheet files

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
+function App (): React.ReactElement {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1)
+          }}
+        >
           count is {count}
         </button>
         <p>
@@ -26,6 +20,12 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        This is a new div to test HMR
+      </p>
+      <p>
+        another test
       </p>
     </div>
   )
