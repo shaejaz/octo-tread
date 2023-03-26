dd948cb73863d47863a9ecac2d1e993dbea125a4
Shahmir Ejaz <shaejaz5@gmail.com>
2023-03-26 17:46:55 +0100

    add base theme object

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
 import router from './routes'
-import { red } from '@mui/material/colors'
 import { CssBaseline } from '@mui/material'
 import { Provider } from 'react-redux'
 import { store } from './services/store'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: red[500],
-    },
-  },
-})
+import { theme } from '@octotread/theme/index'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3c0c75',
+    },
+    secondary: {
+      main: '#125271',
+    },
+    info: {
+      main: '#8643f7',
+    },
+  },
+})
